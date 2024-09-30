26b61a73c5c47537b12b557f84e3b4ba409128b2
mernstackproject <rammohangupta1769@gmail.com>
2024-09-30 08:33:06 +0530

    change in baseurl

--- src/Components/Baseurl.js
+++ src/Components/Baseurl.js
@@ -1,2 +1,2 @@
-export const BaseURL = "https://mern-auth-server-1-yl7j.onrender.com/api/v1" 
+export const BaseURL = "https://mern-auth-server-1-5uk7.onrender.com/api/v1" 
 export let token = localStorage.getItem("auth")
